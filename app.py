ae555bdd9a5f69e63eb31921f22d131e5f94c372
Vitor Sa <vitorsa0602@gmail.com>
2026-07-29 07:35:56 -0500

    feat: add PostgreSQL persistence for webhook events

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os, hmac, subprocess, shutil
+import psycopg2
 
 app = Flask(__name__)
 
@@ -9,23 +10,51 @@ webhook_key = os.getenv("WEBHOOK_SECRET")
 
 @app.route('/deploy', methods=['POST'])
 def deploy():
+  database_url = os.getenv("DATABASE_URL")
+  conn = psycopg2.connect(database_url)
+
+  cur = conn.cursor()
+  cur.execute("INSERT INTO webhook_events (status) VALUES ('pending') RETURNING id;")
+  new_id = cur.fetchone()[0]
+  conn.commit()
+
   auth_header = request.headers.get('Authorization')
 
   if auth_header is None:
+    cur.execute("UPDATE webhook_events SET status = 'unauthorized' , finished_at = NOW() WHERE id = %s", (new_id,))
+
+    conn.commit()
+
     return jsonify({"status": "not received"}), 401
   
   full_key = auth_header.split()
 
   if len(full_key) < 2:
+    cur.execute("UPDATE webhook_events SET status = 'malformed' , finished_at = NOW() WHERE id = %s", (new_id,))
+
+    conn.commit()
+
     return jsonify({"status": "invalid list"}), 400
   
   if hmac.compare_digest(webhook_key, full_key[1]): # comparacao da ssh key verdadeira com o que esta na string full_key
     result = run_deploy()
 
     if result is True:
+      cur.execute("UPDATE webhook_events SET status = 'authorized', finished_at = NOW() WHERE id = %s", (new_id,))
+
+      conn.commit()
+
       return jsonify({"status" : "received"}), 200
     else:
+      cur.execute("UPDATE webhook_events SET status = 'failed', error_message = %s , finished_at = NOW() WHERE id = %s", (result, new_id))
+
+      conn.commit()
+
       return jsonify({"status" : "Internal Server Error"}), 500
+
+  cur.execute("UPDATE webhook_events SET status = 'unauthorized' , finished_at = NOW() WHERE id = %s", (new_id,))
+
+  conn.commit()
   
   return jsonify({"status" : "not received"}), 401
 
