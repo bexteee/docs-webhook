526585ea693ee6d372c8499f4c2ebe72f63ee0e9
Vitor Sa <vitorsa0602@gmail.com>
2026-08-03 12:17:59 -0500

    feat: add RAG indexing script and refactor db connections with context managers

--- app.py
+++ app.py
@@ -11,52 +11,53 @@ webhook_key = os.getenv("WEBHOOK_SECRET")
 @app.route('/deploy', methods=['POST'])
 def deploy():
   database_url = os.getenv("DATABASE_URL")
-  conn = psycopg2.connect(database_url)
 
-  cur = conn.cursor()
-  cur.execute("INSERT INTO webhook_events (status) VALUES ('pending') RETURNING id;")
-  new_id = cur.fetchone()[0]
-  conn.commit()
+  with psycopg2.connect(database_url) as conn:
+    with conn.cursor() as cur:
 
-  auth_header = request.headers.get('Authorization')
+      cur.execute("INSERT INTO webhook_events (status) VALUES ('pending') RETURNING id;")
+      new_id = cur.fetchone()[0]
+      conn.commit()
 
-  if auth_header is None:
-    cur.execute("UPDATE webhook_events SET status = 'unauthorized' , finished_at = NOW() WHERE id = %s", (new_id,))
+      auth_header = request.headers.get('Authorization')
 
-    conn.commit()
+      if auth_header is None:
+        cur.execute("UPDATE webhook_events SET status = 'unauthorized' , finished_at = NOW() WHERE id = %s", (new_id,))
 
-    return jsonify({"status": "not received"}), 401
-  
-  full_key = auth_header.split()
+        conn.commit()
 
-  if len(full_key) < 2:
-    cur.execute("UPDATE webhook_events SET status = 'malformed' , finished_at = NOW() WHERE id = %s", (new_id,))
+        return jsonify({"status": "not received"}), 401
 
-    conn.commit()
+      full_key = auth_header.split()
 
-    return jsonify({"status": "invalid list"}), 400
-  
-  if hmac.compare_digest(webhook_key, full_key[1]): # comparacao da ssh key verdadeira com o que esta na string full_key
-    result = run_deploy()
+      if len(full_key) < 2:
+        cur.execute("UPDATE webhook_events SET status = 'malformed' , finished_at = NOW() WHERE id = %s", (new_id,))
 
-    if result is True:
-      cur.execute("UPDATE webhook_events SET status = 'authorized', finished_at = NOW() WHERE id = %s", (new_id,))
+        conn.commit()
+
+        return jsonify({"status": "invalid list"}), 400
+
+      if hmac.compare_digest(webhook_key, full_key[1]): # comparacao da ssh key verdadeira com o que esta na string full_key
+        result = run_deploy()
+
+        if result is True:
+          cur.execute("UPDATE webhook_events SET status = 'authorized', finished_at = NOW() WHERE id = %s", (new_id,))
+
+          conn.commit()
+
+          return jsonify({"status" : "received"}), 200
+        else:
+          cur.execute("UPDATE webhook_events SET status = 'failed', error_message = %s , finished_at = NOW() WHERE id = %s", (result, new_id))
+
+          conn.commit()
+
+          return jsonify({"status" : "Internal Server Error"}), 500
+
+      cur.execute("UPDATE webhook_events SET status = 'unauthorized' , finished_at = NOW() WHERE id = %s", (new_id,))
 
       conn.commit()
 
-      return jsonify({"status" : "received"}), 200
-    else:
-      cur.execute("UPDATE webhook_events SET status = 'failed', error_message = %s , finished_at = NOW() WHERE id = %s", (result, new_id))
-
-      conn.commit()
-
-      return jsonify({"status" : "Internal Server Error"}), 500
-
-  cur.execute("UPDATE webhook_events SET status = 'unauthorized' , finished_at = NOW() WHERE id = %s", (new_id,))
-
-  conn.commit()
-  
-  return jsonify({"status" : "not received"}), 401
+      return jsonify({"status" : "not received"}), 401
 
 def run_deploy():
   try:
--- index_documents.py
+++ index_documents.py
@@ -1,10 +1,13 @@
 import voyageai
 import os
 from dotenv import load_dotenv
+import psycopg2
 
 load_dotenv()
 voyage_api_key = os.getenv("VOYAGE_API_KEY")
+database_url = os.getenv("DATABASE_URL")
 
+vo = voyageai.Client()
 
 with open("README.md", "r", encoding="utf-8") as readme_file:
     readme_content = readme_file.read()
@@ -27,3 +30,22 @@ for chunk in readme_chunks:
 for chunk in handoff_chunks:
     chunk_content = (chunk, "HANDOFF")
     chunks_list.append(chunk_content)
+
+texts = [chunk[0] for chunk in chunks_list]
+
+response = vo.embed(
+    texts=texts ,
+    model="voyage-3.5-lite" ,
+    input_type="document"
+)
+
+result_embeddings = response.embeddings
+
+with psycopg2.connect(database_url) as conn:
+    with conn.cursor() as cur:
+        for (text, origin), embedding in zip(chunks_list, result_embeddings):
+            cur.execute("INSERT INTO document_chunks (content, source_document, embedding) VALUES (%s, %s, %s);", (text, origin, embedding,))
+
+        conn.commit()
+
+print(f"{len(chunks_list)} chunks inseridos com sucesso.")
